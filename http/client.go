d82dd370b6a30dc883c57044c7e2d55a84618742
RoanBrand <brandroan@gmail.com>
2023-08-31 16:44:01 +0200

    refactor + optimization

--- http/client.go
+++ http/client.go
@@ -3,108 +3,11 @@ package http
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
 	"net/http"
-	"os/exec"
-	"strings"
-	"sync"
 	"time"
 
 	"github.com/RoanBrand/SpectroMonitor/config"
-	"github.com/RoanBrand/SpectroMonitor/displayboard"
-	"github.com/RoanBrand/SpectroMonitor/lights"
-	"github.com/RoanBrand/SpectroMonitor/log"
 )
-
-var furnaceLastResult map[string]time.Duration
-var lock sync.Mutex
-
-func Start(conf *config.Config) {
-	furnaceLastResult = make(map[string]time.Duration)
-
-	go func() {
-		if err := getTime(conf.TimeUpdateUrl); err != nil {
-			log.Println("unable to update time:", err)
-		}
-		time.Sleep(time.Duration(conf.TimeUpdateIntervalSeconds) * time.Second)
-	}()
-
-	url := strings.TrimSuffix(conf.ResultUrl, "/")
-	url += "?"
-	for i := range conf.Furnaces {
-		url += "f=" + conf.Furnaces[i].Name + "&"
-	}
-
-	if conf.TransferSamplesOnly {
-		url += "t=true"
-	}
-
-	go handleDisplayBoards(conf)
-
-	doTask(url, conf)
-	for {
-		time.Sleep(time.Duration(conf.RequestIntervalSeconds) * time.Second)
-		doTask(url, conf)
-	}
-}
-
-func doTask(url string, conf *config.Config) {
-	res, err := getResult(url, conf)
-	if err != nil {
-		log.Println(err)
-	}
-
-	t := time.Now()
-	lock.Lock()
-	defer lock.Unlock()
-
-	for _, f := range conf.Furnaces {
-		for _, resF := range res {
-			if resF.Furnace != f.Name {
-				continue
-			}
-
-			furnaceLastResult[f.Name] = t.Sub(resF.TimeStamp)
-			if int(furnaceLastResult[f.Name].Seconds()) > (conf.FurnaceResultOldTimeMinutes * 60) {
-				// red
-				lights.SetLight(f.LightCardAddress, f.RedLightAddress)
-				lights.ClearLight(f.LightCardAddress, f.GreenLightAddress)
-			} else {
-				// green
-				lights.SetLight(f.LightCardAddress, f.GreenLightAddress)
-				lights.ClearLight(f.LightCardAddress, f.RedLightAddress)
-			}
-
-			break
-		}
-	}
-}
-
-func handleDisplayBoards(conf *config.Config) {
-	colon := true
-	for {
-		time.Sleep(time.Duration(conf.DisplayBoardUpdateRateSeconds) * time.Second)
-		lock.Lock()
-		for _, f := range conf.Furnaces {
-			d, ok := furnaceLastResult[f.Name]
-			if !ok {
-				continue
-			}
-
-			td := time.Duration(conf.FurnaceResultOldTimeMinutes)*time.Minute
-			if d > td {
-				d = td
-			}
-
-			if err := displayboard.Write(f.DisplayBoardAddress, []byte(formatDuration(d, colon))); err != nil {
-				log.Println("error writing to displayboard:", err)
-			}
-		}
-		lock.Unlock()
-		colon = !colon
-	}
-
-}
 
 type resultResponse struct {
 	SampleName string    `json:"sample_name"`
@@ -112,7 +15,7 @@ type resultResponse struct {
 	TimeStamp  time.Time `json:"time_stamp"`
 }
 
-func getResult(url string, conf *config.Config) ([]resultResponse, error) {
+func GetResult(url string, conf *config.Config) ([]resultResponse, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
@@ -133,51 +36,21 @@ func getResult(url string, conf *config.Config) ([]resultResponse, error) {
 	return res, nil
 }
 
-func getTime(url string) error {
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		return errors.New("gettime request error: " + resp.Status)
-	}
-
-	defer resp.Body.Close()
+func GetTime(url string) (time.Time, error) {
 	res := struct {
 		T time.Time `json:"t"`
 	}{}
 
-	dec := json.NewDecoder(resp.Body)
-	if err := dec.Decode(&res); err != nil {
-		return err
+	resp, err := http.Get(url)
+	if err != nil {
+		return res.T, err
 	}
 
-	return setSystemDate(res.T)
-}
-
-func setSystemDate(newTime time.Time) error {
-	_, lookErr := exec.LookPath("date")
-	if lookErr != nil {
-		log.Printf("Date binary not found, cannot set system date: %s\n", lookErr.Error())
-		return lookErr
-	} else {
-		dateString := newTime.Format("2 Jan 2006 15:04:05")
-		log.Printf("Setting system date to: %s\n", dateString)
-		args := []string{"--set", dateString}
-		return exec.Command("date", args...).Run()
+	if resp.StatusCode != http.StatusOK {
+		return res.T, errors.New("gettime request error: " + resp.Status)
 	}
-}
 
-func formatDuration(d time.Duration, withColon bool) string {
-	d = d.Round(time.Minute)
-	h := d / time.Hour
-	d -= h * time.Hour
-	m := d / time.Minute
-
-	if withColon {
-		return fmt.Sprintf("%02d:%02d", h, m)
-	} else {
-		return fmt.Sprintf("%02d %02d", h, m)
-	}
+	defer resp.Body.Close()
+	err = json.NewDecoder(resp.Body).Decode(&res)
+	return res.T, err
 }
--- spectromon/service.go
+++ spectromon/service.go
@@ -1,15 +1,29 @@
 package spectromon
 
 import (
+	"context"
+	"fmt"
+	"os/exec"
+	"strings"
+	"sync"
+	"time"
+
 	"github.com/RoanBrand/SpectroMonitor/config"
 	"github.com/RoanBrand/SpectroMonitor/displayboard"
 	"github.com/RoanBrand/SpectroMonitor/http"
+	"github.com/RoanBrand/SpectroMonitor/lights"
 	"github.com/RoanBrand/SpectroMonitor/log"
 	"github.com/kardianos/service"
 )
 
 type app struct {
 	conf *config.Config
+
+	ctx        context.Context
+	cancelFunc context.CancelFunc
+
+	furnaceLastResult map[string]time.Duration
+	lock              sync.Mutex
 }
 
 func New(c *config.Config) *app {
@@ -17,6 +31,7 @@ func New(c *config.Config) *app {
 }
 
 func (a *app) Start(s service.Service) error {
+	a.ctx, a.cancelFunc = context.WithCancel(context.Background())
 	go a.startup()
 	return nil
 }
@@ -28,9 +43,180 @@ func (a *app) startup() {
 		log.Fatal("could not open serial connection:", err)
 	}
 
-	http.Start(a.conf)
+	a.furnaceLastResult = make(map[string]time.Duration)
+	url := a.makeURL()
+	go a.runGetSetTimeJob()
+	go a.handleDisplayBoards()
+
+	a.doTask(url)
+	interval := time.Duration(a.conf.RequestIntervalSeconds) * time.Second
+	t := time.NewTimer(interval)
+
+	for {
+		select {
+		case <-t.C:
+			a.doTask(url)
+			t.Reset(interval)
+		case <-a.ctx.Done():
+			if !t.Stop() {
+				<-t.C
+			}
+			return
+		}
+	}
 }
 
 func (a *app) Stop(s service.Service) error {
+	a.cancelFunc()
 	return nil
+}
+
+func (a *app) runGetSetTimeJob() {
+	if a.conf.TimeUpdateIntervalSeconds == 0 {
+		return
+	}
+
+	interval := time.Duration(a.conf.TimeUpdateIntervalSeconds) * time.Second
+	t := time.NewTimer(interval)
+
+	for {
+		select {
+		case <-t.C:
+			newTime, err := http.GetTime(a.conf.TimeUpdateUrl)
+			if err != nil {
+				log.Println("unable to get time from network:", err)
+			} else {
+				if err = setSystemDate(newTime); err != nil {
+					log.Println("unable to update system time:", err)
+				}
+			}
+
+			t.Reset(interval)
+		case <-a.ctx.Done():
+			if !t.Stop() {
+				<-t.C
+			}
+			return
+		}
+	}
+}
+
+func (a *app) handleDisplayBoards() {
+	interval := time.Duration(a.conf.DisplayBoardUpdateRateSeconds) * time.Second
+	if interval == 0 {
+		interval = 1
+	}
+
+	t := time.NewTimer(interval)
+	maxAge := time.Duration(a.conf.FurnaceResultOldTimeMinutes) * time.Minute
+	colon := true
+
+	for {
+		select {
+		case <-t.C:
+			a.lock.Lock()
+			for _, f := range a.conf.Furnaces {
+				d, ok := a.furnaceLastResult[f.Name]
+				if !ok {
+					continue
+				}
+
+				if d > maxAge {
+					d = maxAge
+				}
+
+				if err := displayboard.Write(f.DisplayBoardAddress, []byte(formatDuration(d, colon))); err != nil {
+					log.Println("error writing to displayboard:", err)
+				}
+			}
+			a.lock.Unlock()
+			colon = !colon
+
+			t.Reset(interval)
+		case <-a.ctx.Done():
+			if !t.Stop() {
+				<-t.C
+			}
+			return
+		}
+	}
+}
+
+// get latest test samples for furnaces and update lights
+func (a *app) doTask(url string) {
+	res, err := http.GetResult(url, a.conf)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	maxAge := time.Duration(a.conf.FurnaceResultOldTimeMinutes) * time.Minute
+
+	a.lock.Lock()
+	defer a.lock.Unlock()
+
+	now := time.Now()
+	for i := range a.conf.Furnaces {
+		f := &a.conf.Furnaces[i]
+		for j := range res {
+			resF := &res[j]
+
+			if resF.Furnace != f.Name {
+				continue
+			}
+
+			a.furnaceLastResult[f.Name] = now.Sub(resF.TimeStamp)
+			if a.furnaceLastResult[f.Name] > maxAge {
+				// red
+				lights.SetLight(f.LightCardAddress, f.RedLightAddress)
+				lights.ClearLight(f.LightCardAddress, f.GreenLightAddress)
+			} else {
+				// green
+				lights.SetLight(f.LightCardAddress, f.GreenLightAddress)
+				lights.ClearLight(f.LightCardAddress, f.RedLightAddress)
+			}
+
+			break
+		}
+	}
+}
+
+func (a *app) makeURL() string {
+	url := strings.TrimSuffix(a.conf.ResultUrl, "/")
+	url += "?"
+	for i := range a.conf.Furnaces {
+		url += "f=" + a.conf.Furnaces[i].Name + "&"
+	}
+
+	if a.conf.TransferSamplesOnly {
+		url += "t=true"
+	}
+
+	return url
+}
+
+func formatDuration(d time.Duration, withColon bool) string {
+	d = d.Round(time.Minute)
+	h := d / time.Hour
+	d -= h * time.Hour
+	m := d / time.Minute
+
+	if withColon {
+		return fmt.Sprintf("%02d:%02d", h, m)
+	} else {
+		return fmt.Sprintf("%02d %02d", h, m)
+	}
+}
+
+func setSystemDate(newTime time.Time) error {
+	_, err := exec.LookPath("date")
+	if err != nil {
+		log.Printf("Date binary not found, cannot set system date: %s\n", err.Error())
+		return err
+	}
+
+	dateString := newTime.Format("2 Jan 2006 15:04:05")
+	log.Printf("Setting system date to: %s\n", dateString)
+	args := []string{"--set", dateString}
+	return exec.Command("date", args...).Run()
 }
